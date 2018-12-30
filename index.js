be9b71d9c0aa824cdb42ec0e8e54476927d00e27
Jennifer Perez <jenniferperez@pursuit.org>
2018-12-29 21:26:51 -0500

    error messages should display

--- index.js
+++ index.js
@@ -1,38 +1,93 @@
-const express =require('express')
-const math = require('./services/math')
+const express = require('express');
+const math = require('./services/math');
 
-let app = express()
+let app = express();
 
-const port =8000
+const port = 8000;
 
 
-app.get ('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send("Testing")
 })
 
 //----------ADDING
-app.get('/math/add', (req, res)=>{
+app.get('/math/add', (req, res) => {
 
-let values = Object.values(req.query)
+    let values = Object.values(req.query)
+    let if_NAN = values.every((el)=>{
+        return Number(el)
+    })
+    if(if_NAN){
+        res.send({
+                input: req.query,
+                sumString: math.sumString(values),
+                sum: math.getSum(values)
+            })
+    }else{
+       res.send({error: 'Input is not all numbers!!!'})
+    }
 
-console.log(values)
-    res.send({
-        input:(req.query),
-        sumString: math.sumString(values),
-        sum: math.getSum(values)
-      })
-// res.send(req.query)
+
+
+
+    
+
+    // if (typeof values !== "number") {
+    //     res.send("error please input num")
+    // }
+
+    // console.log(values)
+    // res.send({
+    //     input: (req.query),
+    //     sumString: math.sumString(values),
+    //     sum: math.getSum(values)
+    // })
+    // res.send(req.query)
 })
+
+
 
 //-----------MULTIPLYING
-app.get('/math/multiply', (req,res)=>{
-    let newValues=Object.values(req.query)
-// console.log(newValues)
-    res.send({
-            input: (req.query),
-            productString: math.productString(newValues),
-            product: math.getProduct(newValues)
-})
+app.get('/math/multiply/', (req, res) => {
+
+    let newValues = Object.values(req.query);
+    let if_NAN = newValues.every((el)=>{
+        return Number(el)
+    })
+    if(if_NAN){
+        res.send({
+                input: req.query,
+                productString: math.productString(newValues),
+                product: math.getProduct(newValues)
+            })
+    }else{
+       res.send({error: 'Input is not all numbers!!!'})
+    }
+
+   
+
+    // for (let i = 0; i < newValues.length; i++) {
+    //     let entry = newValues[i];
+                
+    //    if (Number(entry)){
+          
+    //         res.send({
+    //             input: newValues,
+    //             productString: math.productString(newValues),
+    //             product: math.getProduct(newValues)
+    //         })
+        
+    //    }else {
+       
+        // res.send({
+        //     input: (req.query),
+        //     productString: math.productString(newValues),
+        //     product: math.getProduct(newValues)
+        // })
+//    }
+//     }
 })
 
+
 app.listen(port)
+
